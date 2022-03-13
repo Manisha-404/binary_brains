68c6a3278c0758260d2a100d5d2e4ff3fad34598
manisha <manishakarus2142@gmail.com>
2022-03-13 14:39:39 +0530

    Updates

---
+++ static/js/614.55b90689.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbinary_brains=self.webpackChunkbinary_brains||[]).push([[614],{1614:function(n,a,s){s.r(a),a.default={}}}]);
+//# sourceMappingURL=614.55b90689.chunk.js.map
